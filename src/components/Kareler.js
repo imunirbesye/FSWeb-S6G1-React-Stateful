fd3a6c0609eb2832c5bf5de4c3741284b0416504
Münir BESYE <imunirbesye@gmail.com>
2023-08-02 22:49:58 +0300

    yapildi

--- src/components/Kareler.js
+++ src/components/Kareler.js
@@ -14,11 +14,11 @@ Herhangi bir noktada yalnızca bir kare aktif olabilir (ya da hiçbiri)
 Aşaıdaki yorumları takip edin.
 */
 
-import React from 'react';
+import React, {useState} from 'react';
 
 
 //Bu değişkeni YALNIZCA bir durum dilimini yüklemek için kullanın!
-const KareIdListesi = ['sqA', 'sqB', 'sqC', 'sqD'];
+const KareIdListesi = ['sqA', 'sqB', 'sqC', 'sqD', 'sqE', 'sqF'];
 
 
 
@@ -27,19 +27,27 @@ export default function Kareler() {
   // 'aktifKare' olmak üzere. Birisi kare idlerini _dizi_ olarak tutacak, diğeri ise aktif olan
   // kareyi gözlemleyecek. Sayfa yüklendiğinde aktif kare olmayacak,
   // yani  'aktifKare' null olmalı.
+  const [aktifKare, setAktifKare] = useState();
+  const [kareler, setKareler] = useState(KareIdListesi);
 	
   const ClassAdiAl = id => {
     // Bu bir click handler değildir, JSX içinde kullanılan bir yardımcıdır(helper).(aşağıya bakın)
-    // Eğer argüman olarak verilen id aktif kare state'indeki id ile eşleşirse, class adı 'active' olan bir string döndürecek
+    // Eğer a+rgüman olarak verilen id aktif kare state'indeki id ile eşleşirse, class adı 'active' olan bir string döndürecek
     // diğer durumlar için boş döndürecek.
     // Etkisini görmek için kareye sağ tıklayın ve "öğeyi inceleyin".
-	return ''
+    if(aktifKare === id)
+      return 'active';
+    else return '';
   };
+
 
   const AktifEt = id => {
     // Bu bir _satır içinden çağırılmış_ click handler yardımcısıdır.
     // id bağımsız değişkenini, stateteki aktif id olacak şekilde ayarlayın
     // eğer zaten aktifse, o zaman önce state i resetlemeliyiz.
+    if(aktifKare === id)
+      setAktifKare('');
+    else setAktifKare(id);
   };
 
   return (
@@ -49,8 +57,8 @@ export default function Kareler() {
         {
           // Kötü bug!  'KareIdListesi' yerine bir state dilimi kullanmalıyız.
           // Şöyle diyebiliriz: "aa bu çalışıyor!" Ama kareler bir state diliminden gelmiyorsa,
-          // asla yeni kare ekleyemeyiz, kareleri düzenleyemeyiz ya da silemeyiz. Düzeltin!
-          KareIdListesi.map(id =>
+          // asla yeni kare ekleyemeyiz, kareleri düzenleyemeyiz ya da silemeyiz. Düzeltin! 
+          kareler.map(id =>
             <div
               id={id}
               key={id}
